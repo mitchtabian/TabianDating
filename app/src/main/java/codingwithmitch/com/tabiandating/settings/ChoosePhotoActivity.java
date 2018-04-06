c8988bb999799ce568fdba19dbb5e6dadc5f156d
Mitch <mitch@tabian.ca>
2018-04-06 16:14:38 -0700

    Module_7.5_End

--- app/src/main/java/codingwithmitch/com/tabiandating/settings/ChoosePhotoActivity.java
+++ app/src/main/java/codingwithmitch/com/tabiandating/settings/ChoosePhotoActivity.java
@@ -6,11 +6,12 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 
 import codingwithmitch.com.tabiandating.R;
 
 
-public class ChoosePhotoActivity extends AppCompatActivity {
+public class ChoosePhotoActivity extends AppCompatActivity implements KeyEvent.Callback{
 
     private static final String TAG = "ChoosePhotoActivity";
     private static final int GALLERY_FRAGMENT = 0;
@@ -51,6 +52,38 @@ public class ChoosePhotoActivity extends AppCompatActivity {
 
     }
 
+
+
+    private void tabRight(){
+        if(mViewPager.getCurrentItem() == 0){
+            mViewPager.setCurrentItem(1);
+        }
+    }
+
+    private void tabLeft(){
+        if(mViewPager.getCurrentItem() == 1){
+            mViewPager.setCurrentItem(0);
+        }
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+                if(event.isCtrlPressed()){
+                    tabLeft();
+                }
+                return false;
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                if(event.isCtrlPressed()){
+                    tabRight();
+                }
+                return false;
+
+            default:
+                return super.onKeyUp(keyCode, event);
+        }
+    }
 }
 
 
---
+++ app/src/main/java/codingwithmitch/com/tabiandating/settings/MyViewPager.java
@@ -0,0 +1,28 @@
+package codingwithmitch.com.tabiandating.settings;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+
+/**
+ * Created by User on 3/30/2018.
+ */
+
+public class MyViewPager extends ViewPager {
+
+    public MyViewPager(@NonNull Context context) {
+        super(context);
+    }
+
+    public MyViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean executeKeyEvent(@NonNull KeyEvent event) {
+        return false;
+    }
+}
